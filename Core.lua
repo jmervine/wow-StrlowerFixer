19306c2c680b2f72f4bcff383957a5e36f2d98a8
Joshua Mervine <jmervine@users.noreply.github.com>
2023-10-23 15:57:47 -0700

    comment the code

--- Core.lua
+++ Core.lua
@@ -1,10 +1,18 @@
+-- Use stdlib 'string.lower' function by default.
 local strlowerf = string.lower
+
+-- If 'strlower' has been previously defined, use that instead.
 if strlower then
     strlowerf = strlower
 end
 
+-- Define a new 'strlower' function.
 function strlower(str)
+    -- Attempt to call the lower function previously defined and determine 
+    -- if there was an error in the call.
     no_error, result = pcall(strlowerf, str)
+
+    -- If there are no errors, return the result.
     if no_error then
         return result
     end

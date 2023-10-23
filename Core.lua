94337fbd6614c8a413f4b0a1ada0c809ac451310
Joshua Mervine <jmervine@users.noreply.github.com>
2023-10-23 16:52:50 -0700

    add onload message and debug flag

--- Core.lua
+++ Core.lua
@@ -1,3 +1,38 @@
+local debug = false
+local frame = CreateFrame("FRAME")
+frame:RegisterEvent("ADDON_LOADED")
+
+function frame:OnEvent(event, arg1)
+    if event == "ADDON_LOADED" and arg1 == "StrlowerFixer" then
+        print("StrlowerFixer loaded, report issues: https://github.com/jmervine/wow-StrlowerFixer.")
+    end
+end
+frame:SetScript("OnEvent", frame.OnEvent);
+
+local function toggleDebug()
+    -- toggle 'debug' mode
+    if debug then
+        print("StrlowerFixer: setting debug to 'false'.")
+        debug = false
+    else
+        print("StrlowerFixer: setting debug to 'true'.")
+        debug = true
+    end
+end 
+
+local function debugPrinter(str)
+    if debug then
+        print("DEBUG{StrlowerFixer}: " .. str)
+    end
+end
+
+SLASH_STRLOWERFIXER1 = "/strlowerfixer"
+SlashCmdList["STRLOWERFIXER"] = function(cmd)
+    if cmd == "debug" then
+        toggleDebug()
+    end
+end
+
 -- Use stdlib 'string.lower' function by default.
 local strlowerf = string.lower
 
@@ -12,11 +47,15 @@ function strlower(str)
     -- if there was an error in the call.
     no_error, result = pcall(strlowerf, str)
 
+    str = str or "nil"
+
     -- If there are no errors, return the result.
     if no_error then
+        debugPrinter("'" .. str .. "' -> '" .. result .. "'.")
         return result
     end
 
+    debugPrinter(" [with error] '" .. str .. "' -> '" .. result .. "'.")
     -- There was an error with the pcall above, so to avoid issues,
     -- we're going to just return the initial call value.
     return str
